c1c7058657b834b46967357239187c659130e0cc
jhh96336 <jhh96336@gmail.com>
2026-05-18 16:44:47 +0800

    d

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="error">
+    <div>The page cannot load. Please copy the URL to the Safari browser to open it</div>
+    <div>ページをロードできませんでしたSafariブラウザにWebアドレスをコピーして開いてください</div>
+  </div>
+</template>
+
+<style>
+.error {
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.error div {
+  margin-bottom: 20px;
+  font-size: 18px;
+  text-align: left;
+  color: #000;
+  font-family: 'Times New Roman', Times, serif;
+  /* font-family: Arial, sans-serif; */
+}
+</style>
